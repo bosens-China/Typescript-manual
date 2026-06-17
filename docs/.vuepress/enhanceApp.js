2cf6d14bb34fa2c6cd45283930b4560749687d43
yliu <yangboses@gmail.com>
2026-06-17 15:04:31 +0800

    refactor: 非首页 body id 由 home 改为 subpage

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,12 +4,12 @@ export default ({ router, isServer }) => {
     if (isServer) {
       return;
     }
-    // 为app添加id
+    // 为非首页路由打标，配合 styles/index.styl 中的 #subpage 媒体查询
     const dom = document.body;
     if (to.path === '/') {
       dom.id = '';
     } else {
-      dom.id = 'home';
+      dom.id = 'subpage';
     }
   });
 };
